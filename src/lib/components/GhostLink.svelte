2ea32bbb4d73ee7a677ecb4afbd99b52e2099873
cubedhuang <99danielh@gmail.com>
2022-08-03 16:19:36 -0400

    feat: add GhostButton and GhostLink components

---
+++ src/lib/components/GhostLink.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let href: string;
+	export let target: string;
+</script>
+
+<a
+	on:click
+	{href}
+	{target}
+	rel={target ? "noopener noreferrer" : undefined}
+	class="border-2 border-white px-4 py-1 rounded-full"
+>
+	<slot />
+</a>
