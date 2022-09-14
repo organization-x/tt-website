3dd205fa131708695b4b8c9b813f7d3241daeb7d
cubedhuang <99danielh@gmail.com>
2022-09-13 21:18:14 -0400

    fix weird imports from internal svelte files

--- src/lib/components/developers/index/DevList.svelte
+++ src/lib/components/developers/index/DevList.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import type { ComponentType } from "svelte/types/runtime/internal/dev.js";
+	import type { ComponentType } from "svelte";
 
 	let icon: ComponentType;
 	export let title: string;
