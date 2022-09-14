37b700859fb712decdce16164c44914d51120d56
cubedhuang <99danielh@gmail.com>
2022-09-13 21:13:55 -0400

    fix: change DevList icon if prop changes

--- src/lib/components/developers/index/DevList.svelte
+++ src/lib/components/developers/index/DevList.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
 	import type { ComponentType } from "svelte/types/runtime/internal/dev.js";
 
-	let Icon: ComponentType;
+	let icon: ComponentType;
 	export let title: string;
 
-	export { Icon as icon };
+	export { icon };
 </script>
 
 <div class="flex font-semibold justify-center items-center gap-2 mt-8">
-	<Icon class="w-6 h-6" />
+	<svelte:component this={icon} class="w-6 h-6" />
 	<h1>{title}</h1>
 </div>
 <div class="flex flex-col gap-4 mt-4">
