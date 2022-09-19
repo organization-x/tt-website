98e4bf4b805e223224b4ecb74a97d10272fa7a07
Urxpaly45 <kalen.shamy@gmail.com>
2022-09-18 23:37:13 -0400

    field form validation

--- src/lib/components/contact/Field.svelte
+++ src/lib/components/contact/Field.svelte
@@ -11,15 +11,50 @@
 
 	const dispatch = createEventDispatcher();
 
+	const email_regex = new RegExp(
+		"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])$"
+	);
+	const all_nums = new RegExp("^[0-9]*$");
+	const website_regex = new RegExp(
+		"((http(s)?)://[(www\\.)?a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*))+$"
+	);
+
+	let isValid = false;
 	let isFilled = false;
 
 	// Only dispatch if the previous state of isFilled is different than the new state.
-	$: isFilled, dispatch("change", { isFilled });
+	$: dispatch("change", { isValid });
 
 	// On input check if the input is filled.
 	const onChange = ({ target }: Event) => {
-		const { value } = target as HTMLInputElement;
+		const { value, name } = target as HTMLInputElement;
 		isFilled = value.length > 0;
+		if (!isFilled) {
+			return;
+		}
+		switch (name) {
+			case "first name":
+			case "last name": {
+				isValid = value[0].toUpperCase() == value[0];
+				break;
+			}
+			case "email": {
+				isValid = email_regex.test(value);
+				break;
+			}
+			case "phone number": {
+				isValid = all_nums.test(value) && value.length <= 15;
+				break;
+			}
+			case "company website": {
+				isValid = website_regex.test(value);
+				break;
+			}
+			default: {
+				isValid = true;
+				break;
+			}
+		}
 	};
 </script>
 
@@ -33,7 +68,8 @@
 	<input
 		on:input={onChange}
 		name={title.toLowerCase()}
-		class:border-green-light={isFilled}
+		class:border-green-light={isFilled && isValid}
+		class:border-red-light={isFilled && !isValid}
 		class:border-transparent={!isFilled}
 		class="w-full h-full px-2 bg-gray-800 flex p-4 mt-2 rounded-lg select-none border-solid border-2 transition-border focus:outline-none"
 		{type}
