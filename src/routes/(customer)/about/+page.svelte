71f8cd22ad589b24be737234e09e4e884b43907b
Said Shafoat <shafoati19@gmail.com>
2022-10-14 04:15:07 +0000

    Add info on Hippo2 card

--- src/routes/(customer)/about/+page.svelte
+++ src/routes/(customer)/about/+page.svelte
@@ -70,7 +70,13 @@
 			class="before:bg-pink-dark"
 			side="right"
 		>
-			Hippo2 is a custom system that a group of 14-19 year-olds built from the ground up in the span of 3 weeks with collaboration across interdisciplinary teams from engineering to design to create a cohesive and seamless experience for customers throughout their journey at AI Camp. It features custom dashboards for students and parents, account creation and linking systems, course registration flows, a full-fledged back office, analytics, and more.
+			Hippo2 is a custom system that a group of 14-19 year-olds built from
+			the ground up in the span of 3 weeks with collaboration across
+			interdisciplinary teams from engineering to design to create a
+			cohesive and seamless experience for customers throughout their
+			journey at AI Camp. It features custom dashboards for students and
+			parents, account creation and linking systems, course registration
+			flows, a full-fledged back office, analytics, and more.
 			<a href="/projects" class="text-teal-light">Projects-Page</a>
 		</Stat>
 		<Stat
