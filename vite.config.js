3e8f11e2504776aec3f7ca5f077df05e9c604b29
Zahtec <32808999+zahtec@users.noreply.github.com>
2022-12-04 22:49:00 -0800

    Vite log level during production

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { defineConfig } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 
 export default defineConfig(({ mode }) => ({
-	plugins: [sveltekit(), wss(mode)]
+	plugins: [sveltekit(), wss(mode)],
+	logLevel: mode === "production" ? "silent" : "info"
 }));
